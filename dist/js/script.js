2c9b02753e617a23c3804aa6cec49e3007ecdc7a
Lilith <winter.rituel@gmail.com>
2020-12-02 12:16:25 +0200

    simple slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -2123,6 +2123,10 @@ window.addEventListener('DOMContentLoaded', () => {
       seconds.textContent = addZero(t.seconds);
 
       if (t.total <= 0) {
+        days.textContent = '00';
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
         clearInterval(timeInterval);
       }
     }
@@ -2301,6 +2305,51 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   fetch('http://localhost:3000/menu').then(data => data.json()).then(result => console.log(result));
+  const slides = document.querySelectorAll('.offer__slide'),
+        prevArrow = document.querySelector('.offer__slider-prev'),
+        nextArrow = document.querySelector('.offer__slider-next'),
+        totalSlides = document.querySelector('#total'),
+        currentSlide = document.querySelector('#current');
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+    totalSlides.textContent = '0' + slides.length;
+  } else {
+    totalSlides.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach(item => {
+      item.style.display = 'none';
+    });
+    slides[slideIndex - 1].style.display = 'block';
+
+    if (slides.length < 10) {
+      currentSlide.textContent = '0' + slideIndex;
+    } else {
+      currentSlide.textContent = slideIndex;
+    }
+  }
+
+  function sliderNav(n) {
+    showSlides(slideIndex += n);
+  }
+
+  prevArrow.addEventListener('click', () => {
+    sliderNav(-1);
+  });
+  nextArrow.addEventListener('click', () => {
+    sliderNav(1);
+  });
 });
 
 /***/ })
